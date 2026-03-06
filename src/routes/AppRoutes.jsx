4e39845ed93df52d1c56bf564d71e40f303b9f75
Meiryanne Martins <meirympereira@gmail.com>
2026-03-06 17:22:46 -0300

    feat(routes): add protected routes with dashboard and auth guard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "../pages/Login"
 import Home from "../pages/Home"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+import Dashboard from "../pages/Dashboard"
+import ProtectedRoute from "../components/ProtectedRoute"
 
 function AppRoutes() {
+
   return (
     <BrowserRouter>
+
       <Routes>
+
+        <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
+
     </BrowserRouter>
   )
 }
